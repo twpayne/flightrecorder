e81d99bfa4f2f00c0a9651b1fea724de0779f205
Tom Payne <twpayne@gmail.com>
2011-04-09 12:55:02 +0200

    Add distutils setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name='sixty15',
+        version='20110409',
+        description='Download tracklogs from Brauniger IQ Basic and Flytec 6015 flight recorders',
+        author='Tom Payne',
+        author_email='twpayne@gmail.com',
+        url='https://github.com/twpayne/6015',
+        scripts=['sixty15'])

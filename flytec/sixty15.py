841bc5704c5607cb2cd8c3530814c3bf257dfd62
Tom Payne <twpayne@gmail.com>
2011-04-09 21:21:51 +0200

    Add RFA command, bug fixes

--- flytec/sixty15.py
+++ flytec/sixty15.py
@@ -26,36 +26,68 @@ from .errors import ProtocolError, ReadError, TimeoutError, WriteError
 from .utils import UTC
 
 
-PA_DeviceNr = 0x00
-PA_DeviceTyp = 0x01
-PA_SoftVers = 0x02
-PA_KalibType = 0x03
-PA_Filt1_K = 0x04
-PA_Filt2_K = 0x05
-PA_Filt4_K = 0x06
-PA_AudioHyst = 0x07
-PA_AudioRsThrFaktor = 0x8
-PA_BattLevel1 = 0x09
-PA_BattLevel2 = 0x0a
-PA_BattLevel2 = 0x0b
-PA_AltiDiff_FLA = 0x0c
-PA_Vario_FLA = 0x0d
-PA_Speed_FLA = 0x0e
-PA_MemoStartDelay = 0x0f
-PA_Vario_FLE = 0x10
-PA_Speed_FLE = 0x11
+FA_FORMAT = {}
+FA_Owner            = 0x00; FA_FORMAT[FA_Owner]            = '<16s'
+FA_AC_Type          = 0x01; FA_FORMAT[FA_AC_Type]          = '<16s'
+FA_AC_ID            = 0x02; FA_FORMAT[FA_AC_ID]            = '<16s'
+FA_Units            = 0x03; FA_FORMAT[FA_Units]            = '<I'
+FA_DiverseFlag      = 0x04; FA_FORMAT[FA_DiverseFlag]      = '<I'
+FA_FiltTyp          = 0x05; FA_FORMAT[FA_FiltTyp]          = '<B'
+FA_Alt1Diff         = 0x06; FA_FORMAT[FA_Alt1Diff]         = '<l'
+FA_VarioDigFk       = 0x07; FA_FORMAT[FA_VarioDigFk]       = '<B'
+FA_BFreqRise        = 0x08; FA_FORMAT[FA_BFreqRise]        = '<I'
+FA_BFreqSink        = 0x09; FA_FORMAT[FA_BFreqSink]        = '<I'
+FA_AudioRise        = 0x0a; FA_FORMAT[FA_AudioRise]        = '<i'
+FA_AudioSink        = 0x0b; FA_FORMAT[FA_AudioSink]        = '<i'
+FA_SinkAlarm        = 0x0c; FA_FORMAT[FA_SinkAlarm]        = '<i'
+FA_FreqGain         = 0x0d; FA_FORMAT[FA_FreqGain]         = '<B'
+FA_PitchGain        = 0x0e; FA_FORMAT[FA_PitchGain]        = '<B'
+FA_MaxRiseRejection = 0x0f; FA_FORMAT[FA_MaxRiseRejection] = '<I'
+FA_VarioMinMaxFk    = 0x10; FA_FORMAT[FA_VarioMinMaxFk]    = '<B'
+FA_RecIntervall     = 0x11; FA_FORMAT[FA_RecIntervall]     = '<B'
+FA_AudioVolume      = 0x12; FA_FORMAT[FA_AudioVolume]      = '<B'
+FA_UTC_Offset       = 0x13; FA_FORMAT[FA_UTC_Offset]       = '<b'
+FA_PressOffset      = 0x14; FA_FORMAT[FA_PressOffset]      = '<l'
+FA_ThermThreshold   = 0x15; FA_FORMAT[FA_ThermThreshold]   = '<i'
+FA_PowerOffTime     = 0x16; FA_FORMAT[FA_PowerOffTime]     = '<B'
+FA_StallSpeed       = 0x1a; FA_FORMAT[FA_StallSpeed]       = '<I'
+FA_WindWheelGain    = 0x1c; FA_FORMAT[FA_WindWheelGain]    = '<B'
+FA_PreThermalThr    = 0x22; FA_FORMAT[FA_PreThermalThr]    = '<i'
 
-PA_FORMAT = ['<I', 'B', '<I', 'B', '4B', '4B', '4B', '4B', '4B', '10C', '10C', '10C', '<l', '<i', 'B', '<i', '<I']
+PA_FORMAT = {}
+PA_DeviceNr         = 0x00; PA_FORMAT[PA_DeviceNr]         = '<I'
+PA_DeviceTyp        = 0x01; PA_FORMAT[PA_DeviceTyp]        = '<B'
+PA_SoftVers         = 0x02; PA_FORMAT[PA_SoftVers]         = '<I'
+PA_KalibType        = 0x03; PA_FORMAT[PA_KalibType]        = '<B'
+PA_Filt1_K          = 0x04; PA_FORMAT[PA_Filt1_K]          = '<4B'
+PA_Filt2_K          = 0x05; PA_FORMAT[PA_Filt2_K]          = '<4B'
+PA_Filt4_K          = 0x06; PA_FORMAT[PA_Filt4_K]          = '<4B'
+PA_AudioHyst        = 0x07; PA_FORMAT[PA_AudioHyst]        = '<4B'
+PA_AudioRsThrFaktor = 0x08; PA_FORMAT[PA_AudioRsThrFaktor] = '<4B'
+PA_BattLevel1       = 0x09; PA_FORMAT[PA_BattLevel1]       = '<10C'
+PA_BattLevel2       = 0x0a; PA_FORMAT[PA_BattLevel2]       = '<10C'
+PA_BattLevel3       = 0x0b; PA_FORMAT[PA_BattLevel2]       = '<10C'
+PA_AltiDiff_FLA     = 0x0c; PA_FORMAT[PA_AltiDiff_FLA]     = '<l'
+PA_Vario_FLA        = 0x0d; PA_FORMAT[PA_Vario_FLA]        = '<i'
+PA_Speed_FLA        = 0x0e; PA_FORMAT[PA_Speed_FLA]        = '<I'
+PA_MemoStartDelay   = 0x0f; PA_FORMAT[PA_MemoStartDelay]   = '<B'
+PA_Vario_FLE        = 0x10; PA_FORMAT[PA_Vario_FLE]        = '<i'
+PA_Speed_FLE        = 0x11; PA_FORMAT[PA_Speed_FLE]        = '<I'
 
 
 class MockSixty15IO(object):
 
     def __init__(self):
+        self.filename = 'mock'
         self.lines = deque()
-        self.fa = [None] * 18
-        self.fa[PA_DeviceNr] = [1234]
-        self.fa[PA_DeviceTyp] = [0]
-        self.fa[PA_SoftVers] = [5678]
+        self.fa = [None] * (FA_PreThermalThr + 1)
+        self.fa[FA_Owner] = ['%-16s' % 'Chrigel Maurer']
+        self.fa[FA_AC_Type] = ['%-16s' % 'Advance Omega']
+        self.fa[FA_AC_ID] = ['%-16s' % 1]
+        self.pa = [None] * (PA_Speed_FLA + 1)
+        self.pa[PA_DeviceNr] = [1234]
+        self.pa[PA_DeviceTyp] = [0]
+        self.pa[PA_SoftVers] = [5678]
         self.tracks = []
         self.tracks.append((
             (0,  9, 11, 16, 12, 43,  3, 1,  0,  8, 53, -161, 978, 452, 3.49, -2.90, 1.38, 'not-set', 'not set', 'not set'), (
@@ -89,13 +121,21 @@ class MockSixty15IO(object):
             (1,  9, 10,  9,  8, 43, 27, 1,  0,  6, 19,    0, 580, 233, 1.90, -2.25, 0.77, 'not-set', 'not-set', 'not-set'), ('G\r\n',)))
 
     def write(self, line):
-        m = re.match(r'\ARPA_([0-9A-F]{2})\r\n\Z', line)
+        m = re.match(r'\ARFA_([0-9A-F]{2})\r\n\Z', line)
         if m:
             index = int(m.group(1), 16)
             if self.fa[index] is None:
                 self.lines.append('No Par\r\n')
             else:
-                self.lines.append('RPA_%02X_%s\r\n' % (index, ''.join('%02X' % ord(c) for c in struct.pack(PA_FORMAT[index], *self.fa[index]))))
+                self.lines.append('RFA_%02X_%s\r\n' % (index, ''.join('%02X' % ord(c) for c in struct.pack(FA_FORMAT[index], *self.fa[index]))))
+            return
+        m = re.match(r'\ARPA_([0-9A-F]{2})\r\n\Z', line)
+        if m:
+            index = int(m.group(1), 16)
+            if self.pa[index] is None:
+                self.lines.append('No Par\r\n')
+            else:
+                self.lines.append('RPA_%02X_%s\r\n' % (index, ''.join('%02X' % ord(c) for c in struct.pack(PA_FORMAT[index], *self.pa[index]))))
             return
         if line == 'ACT_20_00\r\n':
             for track in self.tracks:
@@ -124,6 +164,7 @@ class Sixty15(object):
         self.manufacturer = self.rpa(PA_DeviceTyp)[0]
         self.model = ['6015', 'IQ Basic'][self.manufacturer]
         self.software_version = self.rpa(PA_SoftVers)[0]
+        self.pilot_name = self.rfa(FA_Owner)[0].strip()
 
     def readline(self, timeout=1):
         line = ''
@@ -144,6 +185,17 @@ class Sixty15(object):
     def write(self, line):
         logging.info('write %r' % line)
         self.io.write(line)
+
+    def rfa(self, parameter):
+        self.write('RFA_%02X\r\n' % parameter)
+        line = self.readline(0.1)
+        m = re.match(r'\ARFA_%02X_((?:[0-9A-F]{2})*)\r\n\Z' % parameter, line)
+        if m:
+            return struct.unpack(FA_FORMAT[parameter], ''.join(chr(int(x, 16)) for x in re.findall(r'..', m.group(1))))
+        elif line == 'No Par\r\n':
+            return None
+        else:
+            raise ProtocolError('unexpected response %r' % line)
 
     def rpa(self, parameter):
         self.write('RPA_%02X\r\n' % parameter)
@@ -201,6 +253,7 @@ class Sixty15(object):
         return {
             'manufacturer': ['Flytec', 'Brauniger'][self.manufacturer],
             'model': self.model,
+            'pilot_name': self.pilot_name,
             'serial_number': self.serial_number,
             'software_version': self.software_version}
 

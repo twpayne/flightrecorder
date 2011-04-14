ddf196bf51f826134e2250a0537e6d5a70ecc3ee
Tom Payne <twpayne@gmail.com>
2011-04-14 12:17:47 +0200

    Bump version and install flytec-gui

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 
 setup(
         name='flytec-utils',
-        version='20110409',
+        version='20110414',
         description='Utilities for Flytec and Brauniger flight recorders',
         author='Tom Payne',
         author_email='twpayne@gmail.com',
         url='https://github.com/twpayne/flytec-utils',
         packages=['flytec'],
-        scripts=['scripts/flytec'])
+        scripts=['scripts/flytec', 'scripts/flytec-gui'])

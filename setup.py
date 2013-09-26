df9a88efd43ea0580d32a1a4f157b0820ab38e6b
Tom Payne <twpayne@gmail.com>
2013-09-27 00:11:52 +0200

    Remove long description and bump version

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 
-import os
 from setuptools import setup
 
 
-version = '0.9.2'
-
-long_description = open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst')).read()
+version = '0.9.3'
 
 setup(
         author='Tom Payne',
         author_email='twpayne@gmail.com',
         description='Utilities for flight recorders',
-        long_description=long_description,
         name='flightrecorder',
         packages=['flightrecorder'],
         scripts=['scripts/flightrecorder'],
